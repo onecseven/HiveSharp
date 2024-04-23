60149a89db62d90dc9bf2601cdedbc9ed458ff85
Tati <iced.tati@gmail.com>
2024-04-23 17:26:10 -0400

    more fragmentation

---
+++ Base/Path.cs
@@ -0,0 +1,62 @@
+﻿using Hive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive
+{
+    public class Path
+    {
+        public List<Cell> steps;
+        public Cell last
+        {
+            get
+            {
+                if (steps.Count < 1) throw new ArgumentOutOfRangeException("path class");
+                return steps[steps.Count - 1];
+            }
+        }
+        public Cell penult
+        {
+            get
+            {
+                if (steps.Count < 2) throw new ArgumentOutOfRangeException("path class");
+                return steps[steps.Count - 2];
+            }
+        }
+        public bool isSingleStep { get => steps.Count == 1; }
+        public List<(Cell first, Cell last)> pairs
+        {
+            get
+            {
+                if (isSingleStep) { return new List<(Cell first, Cell last)>(); }
+                List<(Cell first, Cell last)> result = new List<(Cell first, Cell last)>();
+                for (int i = 0; i < steps.Count - 2; i++)
+                {
+                    //int next = i + 1 > steps.Count - 1 ? 0 : i + 1;
+                    result.Add((steps[i], steps[i + 1]));
+                }
+                return result;
+            }
+        }
+        public Pieces pathType;
+        public Path(List<Cell> steps, Pieces type)
+        {
+            this.steps = steps;
+            this.pathType = type;
+        }
+        public bool isNullPath { get => steps.Count == 0; }
+        public Path(Cell step, Pieces type)
+        {
+            this.steps = new List<Cell>() { step };
+            this.pathType = type;
+        }
+        public Path(params Cell[] step)
+        {
+            this.steps = step.ToList();
+        }
+    }
+
+}
---
+++ Base/Tile.cs
@@ -0,0 +1,38 @@
+﻿using Hive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive
+{
+    public class Tile
+    {
+
+        public Cell cell;
+        public List<Piece> pieces = new List<Piece>();
+        public Piece activePiece
+        {
+            get
+            {
+                if (pieces.Count > 0) return pieces[0];
+                else return null;
+            }
+        }
+        public int zIndex => isOccupied ? pieces.Count : 0;
+        public bool hasBlockedPiece => pieces.Count > 1;
+        public bool isOccupied => pieces.Count > 0;
+        public Tile(Cell cell)
+        {
+            this.cell = cell;
+        }
+        public void addPiece(Piece piece) { pieces.Insert(0, piece); }
+        public Piece removePiece()
+        {
+            Piece returnable = pieces[0];
+            pieces.RemoveAt(0);
+            return returnable;
+        }
+    }
+}
